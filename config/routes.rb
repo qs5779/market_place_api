cd0f459c3073374b8747a6d28f0de09277c7d7c3
Quien Sabe <qs5779@mail.com>
2020-08-26 08:07:21 -0400

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+  # We are going to list our resources here
+  # stopped on page 20
+  end
 end
